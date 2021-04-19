96a0c0bff442ff6e890b3bd9e9209ef5cc1da976
Rodrigo Loy <rodrigohp4@hotmail.com>
2021-04-19 17:21:25 -0500

    Creación de pagina ASP.NET

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dotnet_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
